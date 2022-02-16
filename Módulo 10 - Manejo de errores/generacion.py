7b47e2bcd0ca29c883d367b4a2a9fec3820c82fe
LeafarOdagled01~ <rafael1del@gmail.com>
2022-02-15 22:56:09 -0600

    actualizacion

--- Módulo 10 - Manejo de errores/generacion.py
+++ Módulo 10 - Manejo de errores/generacion.py
@@ -1,8 +1,4 @@
-def water_left(astronauts, water_left, days_left):
-    daily_usage = astronauts * 11
-    total_usage = daily_usage * days_left
-    total_water_left = water_left - total_usage
-    return f"Total water left after {days_left} days is: {total_water_left} liters"
+
 
 
 def water_left(astronauts, water_left, days_left):
@@ -13,4 +9,7 @@ def water_left(astronauts, water_left, days_left):
         raise RuntimeError(f"There is not enough water for {astronauts} astronauts after {days_left} days!")
     return f"Total water left after {days_left} days is: {total_water_left} liters"
 
-water_left(5, 100, 2)
+try:
+    water_left(5, 100, 2)
+except RuntimeError as err:
+    alert_navigation_system(err)
--- Módulo 3 - Usar lógica booleana/kata3.py
+++ Módulo 3 - Usar lógica booleana/kata3.py
@@ -9,10 +9,15 @@ if velocidad>25:
 else: 
     print("no hay pleigro")
     
+    
+    
 if velocidad2>=20: 
     print("¡hay uno que se dirige a la tierra ahora a una velociadad de "+ str(velocidad2)+"km/s!")
 else: 
+    
     print("no hay pleigro")
+    
+    
     
 if velocidad3>25 and Size>25:
     print("¡peligro hay un asteroide que se dirige a la tierra!")
